74804bdc351e5472292543603fb1fb62d6d8d165
achilles1801 <124203464+achilles1801@users.noreply.github.com>
2023-08-09 15:45:16 -0400

    Update GraphAlgorithms.java

--- homework8/hw8/src/GraphAlgorithms.java
+++ homework8/hw8/src/GraphAlgorithms.java
@@ -14,7 +14,6 @@ import java.util.Set;
  * @author Majd Khawaldeh
  * @version 1.0
  * @userid majd
- * @GTID 903677841
  * Collaborators: LIST ALL COLLABORATORS YOU WORKED WITH HERE
  * <p>
  * Resources: LIST ALL NON-COURSE RESOURCES YOU CONSULTED HERE
